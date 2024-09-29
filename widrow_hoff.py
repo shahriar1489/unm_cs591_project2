357d24852c49d6b0e33aea1e9fd0295ee2f741dc
Upadhayaya, Rajesh <upadraj@gatech.edu>
2024-09-29 14:25:07 -0600

    Decreased the loss.

--- widrow_hoff.py
+++ widrow_hoff.py
@@ -1,27 +1,33 @@
 import numpy as np
-
-
 class Widrow:
     def __init__(self, features):
         np.random.seed(0)
-        self.weight = np.random.rand(features)
+        self.weights = np.random.rand(features) * 0.05
         self.bias = 0
 
     def forward(self, input):
-        output = np.dot(self.weight, input) + self.bias
+        output = np.dot(input, self.weights) + self.bias
+        return output
 
-    def fit(self, X, y, epochs=10, lr=1e-6):
+    def fit(self, X, y, epochs=10, lr=1e-12):
         observations, features = X.shape
 
         # training
-        for epoch in epochs:
+        for epoch in range(epochs):
             total_loss = 0
             for o in range(observations):
                 y_hat = self.forward(X[o])
-                loss = (y[o], y_hat) ** 2
 
-                self.weights += 2 * lr * loss * X[o]
-                self.bias += 2 * lr * loss
-                total_loss += loss
+                # Error
+                error = y[o] - y_hat
 
-            print(f"For Epoch{epoch +1}/ {epochs}, Loss: {total_loss}")
+                # weights and bias  update
+                self.weights += lr * error * X[o]
+                self.bias += lr * error
+
+                # squared error for loss
+                total_loss += error ** 2  
+
+            # average loss
+            avg_loss = total_loss / observations
+            print(f"Epoch {epoch + 1}/{epochs}, Loss: {avg_loss}")
